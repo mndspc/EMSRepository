b5f2b19c65345a49560237e34b5a9899ff5e1674
Shoaib <shoaib.ghachi@gmail.com>
2022-06-16 15:41:03 +0530

    interface IEmpProfileDAL and class EmpProfileDAL added

---
+++ EMS/DAL/EmpProfileDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL
+{
+    internal class EmpProfileDAL : IEmpProfileDAL<EmpProfile>
+    {
+        public bool DeleteEmployee(int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveEmployee(EmpProfile empProfile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEmployee(EmpProfile empProfile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EMS/DAL/IEmpProfileDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL
+{
+    public  interface IEmpProfileDAL<EmpProfile>
+    {
+        bool SaveEmployee(EmpProfile empProfile);
+
+        bool DeleteEmployee(int code);
+
+        bool UpdateEmployee(EmpProfile empProfile);
+    }
+}
